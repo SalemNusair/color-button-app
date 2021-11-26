3a2011bcc8ce7f97aebf40e39bfaa707e2f05651
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2021-11-26 02:23:13 +0200

    feat: create first test for button color clicked

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,15 @@ import { render, screen,fireEvent } from '@testing-library/react';
 import App from './App';
 
 test("Button has correct initial color",()=>{
+  // to render the App component
   render(<App/>);
+  // getByRole it has the first argument is html tag and the second element is obj which normal the prop for tag
+  // `name` is the text between button
   const buttonColorElement=screen.getByRole("button",{name:"change to blue"});
+  // expect is a assertion which hav toHaveStyle which use to check if the background of the button has red color
+  // otherwise it will fail
   expect(buttonColorElement).toHaveStyle({backgroundColor:"red"});
+  // `fireEvent` it uses for events as the shown below as well the rest code below is used when the button clicked
   fireEvent.click(buttonColorElement);
   expect(buttonColorElement).toHaveStyle({backgroundColor:"blue"});
   expect(buttonColorElement.textContent).toBe("change to red");
