a1b996eb5a49ac034fdd4b53d9761d8735e702a5
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2021-11-26 02:22:43 +0200

    feat: create first test for button color clicked

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Button has correct initial color",()=>{
+  render(<App/>);
+  const buttonColorElement=screen.getByRole("button",{name:"change to blue"});
+  expect(buttonColorElement).toHaveStyle({backgroundColor:"red"});
+  fireEvent.click(buttonColorElement);
+  expect(buttonColorElement).toHaveStyle({backgroundColor:"blue"});
+  expect(buttonColorElement.textContent).toBe("change to red");
+
+})
+
