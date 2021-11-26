add77281ad9d690b391809a6d05413fa435f571a
SalemNusair <52820565+SalemNusair@users.noreply.github.com>
2021-11-27 00:58:49 +0200

    Feature: Exam 1 for testing and lessons learned (#2)
    * feat: disable button when checkbox checked test
    
    * feat: disable button when checkbox checked functionality
    
    * refactor: test + code

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,38 @@
-import { render, screen,fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test("Button has correct initial color",()=>{
+test("Button has correct initial color", () => {
   // to render the App component
-  render(<App/>);
+  render(<App />);
   // getByRole it has the first argument is html tag and the second element is obj which normal the prop for tag
   // `name` is the text between button
-  const buttonColorElement=screen.getByRole("button",{name:"change to blue"});
+  const buttonColorElement = screen.getByRole("button", {
+    name: "change to blue",
+  });
   // expect is a assertion which hav toHaveStyle which use to check if the background of the button has red color
   // otherwise it will fail
-  expect(buttonColorElement).toHaveStyle({backgroundColor:"red"});
+  expect(buttonColorElement).toHaveStyle({ backgroundColor: "red" });
   // `fireEvent` it uses for events as the shown below as well the rest code below is used when the button clicked
   fireEvent.click(buttonColorElement);
-  expect(buttonColorElement).toHaveStyle({backgroundColor:"blue"});
+  expect(buttonColorElement).toHaveStyle({ backgroundColor: "blue" });
   expect(buttonColorElement.textContent).toBe("change to red");
+});
+test("intial check", () => {
+  render(<App />);
+  // check if the button is enabled
+  const buttonColor = screen.getByRole("button", { name: "change to blue" });
+  expect(buttonColor).toBeEnabled();
+  // find checbox
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
 
-})
+test("test checkbox", () => {
+  render(<App />);
+  const buttonColor = screen.getByRole("button");
+  const checbox = screen.getByRole("checkbox");
 
+  fireEvent.click(checbox);
+  expect(checbox).toBeChecked();
+  expect(buttonColor).toBeDisabled();
+});
